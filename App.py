d0b5ea380783a51021c889fa0db36f14232b2d61
PP300 <77737944+PP300@users.noreply.github.com>
2021-01-20 21:54:53 +0700

    My first coding Python
    I run the programme already but it's doesn't show me the result.

---
+++ App.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title('JustPython')
+window.minsize(width=400, height=400)
+
+title_label = tk.Label(master=window, text='สูตรคูณแม่')
+title_label.pack()
+
+number_input = tk.Entry(master=window)
+number_input.pack()
+
+go_button = tk.Button(master=window, text='ได้แก่')
+go_button.pack()
+
+output_label = tk.Label(master=window)
+output_label.pack()
